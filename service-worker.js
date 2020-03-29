61f641736104281a93bae39e918bc102775482c8
morsingnet <morsingnet@users.noreply.github.com>
2020-03-29 22:24:46 +0000

    Deploying to gh-pages from  - 60455fa9349bee9cd7b7ff7cf5d7de4d2dc75a5d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/codex.academy/precache-manifest.ff38660fd8416f526a0888bea018ddf0.js"
+  "/precache-manifest.7cf8142e9e163d4dde11044076ed428d.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "codex-academy-website"});
